676bda9653146b5ead798fe4f769e05492febbe2
Matthew Sells <matthew.m.sells@gmail.com>
2022-01-18 18:14:24 -0600

    use custom auth route handling

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -3,28 +3,65 @@ import passport from 'passport';
 
 import { createJwtToken } from '@/utils/auth';
 import { format } from '@/utils/response';
+import ServerError from '@/lib/error/ServerError';
 
 const router = Router();
 
 router.post(
 	'/sign_up',
-	passport.authenticate('sign-up', { session: false }),
+	(req, res, next) => {
+		passport.authenticate('sign-up', (err, user) => {
+			if (err) {
+				return next(new ServerError(500, 'error', err.message));
+			}
+
+			req.logIn(user, { session: false }, (err) => {
+				if (err) {
+					next(err);
+				}
+
+				next();
+			});
+		})(req, res, next);
+	},
 	respondWithToken,
 );
 
 router.post(
 	'/sign_in',
-	passport.authenticate('sign-in', { session: false }),
+	(req, res, next) => {
+		passport.authenticate('sign-in', (err, user) => {
+			if (err) {
+				return next(new ServerError(500, 'error', err.message));
+			}
+
+			if (!user) {
+				return next(
+					new ServerError(401, 'fail', 'User not found', {
+						user: 'Invalid email or password',
+					}),
+				);
+			}
+
+			req.logIn(user, { session: false }, (err) => {
+				if (err) {
+					next(err);
+				}
+
+				next();
+			});
+		})(req, res, next);
+	},
 	respondWithToken,
 );
 
 export default router;
 
-function respondWithToken(req: Request, res: Response): void {
-	if (!req.user) {
+function respondWithToken({ user }: Request, res: Response): void {
+	if (!user) {
 		throw new Error('No user present in request');
 	}
 
-	const token = createJwtToken(req.user);
-	res.json(format.success({ token, user: req.user }));
+	const token = createJwtToken(user);
+	res.json(format.success({ token, user }));
 }
